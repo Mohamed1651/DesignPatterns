aa9d4296fd0280c116d23c244b059677913ab71e
Mohamed Abdi <khadar.abdi165@gmail.com>
2025-04-04 13:14:13 +0200

    composite pattern implemented

---
+++ Structural/Composite/File.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Composite
+{
+    public class File : FileSystemItem
+    {
+        private int size;
+        public File(string name, int size) : base(name)
+        {
+            this.size = size;
+        }
+
+        public override int GetSize()
+        {
+            return size;
+        }
+    }
+}
---
+++ Structural/Composite/Folder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Composite
+{
+    public class Folder : FileSystemItem
+    {
+        private List<FileSystemItem> items = new List<FileSystemItem>();
+
+        public Folder(string name) : base(name) { }
+
+        public void Add(FileSystemItem item)
+        {
+            items.Add(item);
+        }
+
+        public override int GetSize()
+        {
+            int total = 0;
+            foreach (var item in items)
+            {
+                total += item.GetSize();
+            }
+            return total;
+        }
+    }
+}
