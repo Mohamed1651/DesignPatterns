7fab3a59c72bc376bf2db3041a36a4b17527fcf7
Mohamed Abdi <khadar.abdi165@gmail.com>
2025-04-05 16:04:12 +0200

    Strategy Pattern implemented

---
+++ Behavioral/Strategy/PayPalPayment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Strategy
+{
+    public class PayPalPayment : IPaymentStrategy
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount} using PayPal.");
+        }
+    }
+}
