2ab49a4fa891cfcef31499a90eb6ac879012fe2d
Mohamed Abdi <khadar.abdi165@gmail.com>
2025-04-05 16:23:07 +0200

    Observer pattern implemented. Stocks example

---
+++ Behavioral/Observer/Stock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Observer
+{
+    public class Stock : IStock
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+        private decimal _price;
+
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                NotifyObservers();
+            }
+        }
+
+        public void AddObserver(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            _observers.ForEach(observer => observer.Update(_price));
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+    }
+}
