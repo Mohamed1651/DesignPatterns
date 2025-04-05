f0e3bd7beb7bcb3052c1de7dbe363cda0973cf9a
Mohamed Abdi <khadar.abdi165@gmail.com>
2025-04-05 21:48:52 +0200

    Iterator pattern implemented using book example.

---
+++ Behavioral/Iterator/BookCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Iterator
+{
+    public class BookCollection : IBookCollection
+    {
+        private List<Book> books = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public IBookIterator CreateIterator()
+        {
+            return new BookIterator(books);
+        }
+    }
+}
---
+++ Behavioral/Iterator/BookIterator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Iterator
+{
+    public class BookIterator : IBookIterator
+    {
+        private List<Book> books;
+        private int currentIndex = 0;
+
+        public BookIterator(List<Book> books)
+        {
+            this.books = books;
+        }
+        public bool HasNext()
+        {
+            return currentIndex < books.Count;
+        }
+
+        public Book Next()
+        {
+            return books[currentIndex++];
+        }
+    }
+}
