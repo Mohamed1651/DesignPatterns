1ab9b5caf0ab3becbd280fa8f7d4c304e6eecf03
Mohamed Abdi <khadar.abdi165@gmail.com>
2025-04-06 13:39:29 +0200

    Mediator pattern using chatroom example.

---
+++ Behavioral/Mediator/ChatRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Mediator
+{
+    public class ChatRoom : IChatRoomMediator
+    {
+        public void ShowMessage(User user, string message)
+        {
+            Console.WriteLine($"{DateTime.Now} [{user.name}]: {message}");
+        }
+    }
+}
