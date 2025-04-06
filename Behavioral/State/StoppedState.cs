32f84bc3400f230ab73e76172b32f1c933484503
Mohamed Abdi <khadar.abdi165@gmail.com>
2025-04-06 12:46:09 +0200

    state pattern implemented

---
+++ Behavioral/State/StoppedState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.State
+{
+    public class StoppedState : IPlayerState
+    {
+        public void Play(MediaPlayer mediaPlayer)
+        {
+            Console.WriteLine("Starting playback.");
+            mediaPlayer.SetState(new PlayingState());
+        }
+
+        public void Pause(MediaPlayer mediaPlayer)
+        {
+            Console.WriteLine("Cannot pause. Media is stopped.");
+        }
+
+        public void Stop(MediaPlayer mediaPlayer)
+        {
+            Console.WriteLine("Already stopped.");
+        }
+    }
+}
